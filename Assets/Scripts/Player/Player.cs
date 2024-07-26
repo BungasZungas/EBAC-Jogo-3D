6412ade2cabe7bb6075a254779807bcb07ac10f6
BungasZungas <caiquefloureiro@gmail.com>
2024-07-26 02:40:03 -0300

    add clothes

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using ZUNGAS.Core.Singleton;
+using Cloth;
 
 public class Player : Singleton<Player>//, IDamageable
 {
@@ -28,6 +29,9 @@ public class Player : Singleton<Player>//, IDamageable
     [Header("Life")]
     public HealthBase healthBase;
     public UIFillUpdater uiGunUpdater;
+
+    [Space]
+    [SerializeField] private ClothChanger _clothChanger;
 
     private bool _alive = true;
 
@@ -131,5 +135,30 @@ public class Player : Singleton<Player>//, IDamageable
         {
             transform.position = CheckPointManager.Instance.GetPositionFromLastCheckPoint();
         }
+    }
+
+    public void ChangeSpeed(float speed, float duration)
+    {
+        StartCoroutine(ChangeSpeedCoroutine(speed, duration));
+    }
+
+    IEnumerator ChangeSpeedCoroutine(float localSpeed, float duration)
+    {
+        var defaultSpeed = speed;
+        speed = localSpeed;
+        yield return new WaitForSeconds(duration);
+        speed = defaultSpeed;
+    }
+
+    public void ChangeTexture(ClothSetup setup, float duration)
+    {
+        StartCoroutine(ChangeTextureCoroutine(setup, duration));
+    }
+
+    IEnumerator ChangeTextureCoroutine(ClothSetup setup, float duration)
+    {
+        _clothChanger.ChangeTexture(setup);
+        yield return new WaitForSeconds(duration);
+        _clothChanger.ResetTexture();
     }
 }
